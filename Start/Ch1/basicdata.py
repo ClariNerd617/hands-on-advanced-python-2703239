a04239cc98b2574de3e832c2bac1b668ba3ec9c3
Henry Goodwin <5322154+ClariNerd617@users.noreply.github.com>
2024-03-23 20:08:11 +0000

    updates

--- Start/Ch1/basicdata.py
+++ Start/Ch1/basicdata.py
@@ -2,21 +2,19 @@
 # Introspect the data to make some determinations
 
 import json
-import pprint
+from pprint import pp
+
 
 # open the sample weather data file and use the json module to load and parse it
 with open("../../sample-weather-history.json", "r") as weatherfile:
     weatherdata = json.load(weatherfile)
 
 # TODO: What was the warmest day in the data set?
-
 max_temps = [day["tmax"] for day in weatherdata]
-# print(max(max_temps))
-print([day['date'] for day in weatherdata if day['tmax'] == 102])
+pp([day['date'] for day in weatherdata if day['tmax'] == 102])
 # TODO: What was the coldest day in the data set?
 min_temps = [day["tmin"] for day in weatherdata]
-# print(min(min_temps))
-print([day['date'] for day in weatherdata if day['tmin'] == min(min_temps)])
+pp([day['date'] for day in weatherdata if day['tmin'] == min(min_temps)])
 
 # TODO: How many days had snowfall?
 snow_days = [day['date'] for day in weatherdata if day['snow'] != 0.0]
