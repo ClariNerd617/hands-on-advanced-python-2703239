f87fed2e1cb0d3b68235864de5ff28aa7321b8d8
Henry Goodwin <5322154+ClariNerd617@users.noreply.github.com>
2024-03-23 00:31:38 +0000

    started the lesson

--- Start/Ch1/basicdata.py
+++ Start/Ch1/basicdata.py
@@ -10,9 +10,14 @@ with open("../../sample-weather-history.json", "r") as weatherfile:
 
 # TODO: What was the warmest day in the data set?
 
-
+max_temps = [day["tmax"] for day in weatherdata]
+# print(max(max_temps))
+print([day['date'] for day in weatherdata if day['tmax'] == 102])
 # TODO: What was the coldest day in the data set?
-
+min_temps = [day["tmin"] for day in weatherdata]
+# print(min(min_temps))
+print([day['date'] for day in weatherdata if day['tmin'] == min(min_temps)])
 
 # TODO: How many days had snowfall?
-
+snow_days = [day['date'] for day in weatherdata if day['snow'] != 0.0]
+print(len(snow_days))
